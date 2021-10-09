5f8e75c54b36ba45436175ac8de6669a37c2a29a
Jeba Suthan <itjebasuthan@gmail.com>
2021-10-10 01:16:39 +0530

    Github pages added

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:Jebasuthan.github.io.git main
+# git push -f git@github.com:Jebasuthan.github.io main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:Jebasuthan/vue_crud_bootstrap.git main:gh-pages
