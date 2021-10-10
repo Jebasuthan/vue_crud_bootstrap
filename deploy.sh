38ba0fa9e56c5071134ccf24103f5d8dd1fe4a0a
Jeba Suthan <itjebasuthan@gmail.com>
2021-10-10 15:14:41 +0530

    Github pages deployment script added

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:jebasuthan/hello-world.git master:gh-pages
+git push -f git@github.com:Jebasuthan/vue_crud_bootstrap.git master:gh-pages
 cd -
