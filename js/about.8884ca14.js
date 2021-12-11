b9924a4b0663e36b04ea3a6d6121a723deceba6e
Jeba Suthan <itjebasuthan@gmail.com>
2021-12-11 08:20:41 +0530

    deploy

---
+++ js/about.8884ca14.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(e,t,n){"use strict";n.r(t);var u=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},a=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",[n("h3",[e._v("This is the About Page")])])}],r={name:"about"},s=r,c=n("2877"),i=Object(c["a"])(s,u,a,!1,null,null,null);t["default"]=i.exports}}]);
+//# sourceMappingURL=about.8884ca14.js.map
